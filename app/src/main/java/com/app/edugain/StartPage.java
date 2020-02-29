9433d7ba685f483e396bdafbc96fc22258c6cb18
DivakarChowdary <57917417+divakarchowdary@users.noreply.github.com>
2020-02-28 19:37:48 -0600

    test

---
+++ app/src/main/java/com/app/edugain/StartPage.java
@@ -0,0 +1,27 @@
+package com.app.edugain;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartPage extends AppCompatActivity {
+
+    private static int splashTime = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(StartPage.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        }, splashTime);
+    }
+}
